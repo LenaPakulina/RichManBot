07962dc8e36e17c5acf7b2a68fdf32270797903e
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-11 16:52:20 +0500

    Initial commit

---
+++ src/main/java/ru/pakula/bot/RichManBotApplication.java
@@ -0,0 +1,13 @@
+package ru.pakula.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichManBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichManBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pakula/bot/RichManBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pakula.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichManBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

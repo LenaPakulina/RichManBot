6ed81918fdc250f9be751d6d820ea28209b6a68a
Elena Pakulina <el.pakulina@yandex.ru>
2024-02-12 10:04:56 +0500

    Добавлены кнопки к сообщениям, с редактированием msg

--- src/main/java/ru/pakula/bot/service/TelegramBot.java
+++ src/main/java/ru/pakula/bot/service/TelegramBot.java
@@ -1,16 +1,20 @@
 package ru.pakula.bot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.pakula.bot.config.BotConfig;
@@ -67,12 +71,76 @@ public class TelegramBot extends TelegramLongPollingBot {
                     registerPerson(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+                case "/register":
+                    register(chatId);
+                    break;
                 case "/help":
                     sendMessage(chatId, HELP_TEXT);
                     break;
                 default:
                     sendMessage(chatId, "Sorry, command was not recognized.");
             }
+        } else if (update.hasCallbackQuery()) {
+            String callBackData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callBackData.equals("YES_BUTTON")) {
+                String text = "You pressed YES button;";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chatId));
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error #0003: " + e.getMessage());
+                }
+
+            } else if (callBackData.equals("NO_BUTTON")) {
+                String text = "You pressed NO button;";
+                EditMessageText message = new EditMessageText();
+                message.setChatId(String.valueOf(chatId));
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    log.error("Error #0003: " + e.getMessage());
+                }
+            }
+        }
+
+    }
+
+    private void register(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Do you really want to register?");
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        var yes_btn = new InlineKeyboardButton();
+        yes_btn.setText("Yes");
+        yes_btn.setCallbackData("YES_BUTTON");
+
+        var no_btn = new InlineKeyboardButton();
+        no_btn.setText("No");
+        no_btn.setCallbackData("NO_BUTTON");
+
+        row.add(yes_btn);
+        row.add(no_btn);
+        rowsInLine.add(row);
+        markup.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error #0003: " + e.getMessage());
         }
     }
 
